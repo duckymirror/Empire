0403a024239c3618b48fcf6258a6366c8a0be3d6
Igor Shaposhnikov <jourloy@yandex.ru>
2019-11-07 22:13:03 +0300

    Переписано поведение

--- role.claimer.js
+++ role.claimer.js
@@ -5,57 +5,25 @@ var roleClaimer = {
             creep.memory.room = creep.room.name;
         }
 
-        if (creep.memory.room == "W49S28") {
-            creep.memory.workMode = 0;
-        } else if (creep.memory.room == "W49S28") {
-            creep.memory.workMode = 1;
-        } else if (creep.memory.room == "W49S28") {
-            creep.memory.workMode = 2;
+        var targetRoom = "W48S27"
+
+
+        if (creep.room.name != targetRoom) {
+          creep.moveTo(new RoomPosition(25, 25, targetRoom));
+        } else if (creep.room.name == targetRoom) {
+          if(creep.room.controller && !creep.room.controller.my) {
+            if(creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(creep.room.controller);
+            }
+          } else {
+            if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(creep.room.controller);
+            }
+            if(creep.signController(creep.room.controller, "Room under control") == ERR_NOT_IN_RANGE) {
+              creep.moveTo(creep.room.controller);
+            }
+          }
         }
-
-        switch (creep.memory.workMode) {
-            case 0:
-                if (creep.room.name != "W49S28") {
-                    creep.moveTo(new RoomPosition(25, 25, "W49S28"));
-                } else if (creep.room.name == "W49S28") {
-                    if (!creep.room.controller.my) {
-                        if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(creep.room.controller);
-                        }
-                    } else {
-                      if(creep.room.controller) {
-                          if(creep.signController(creep.room.controller, "Peace player") == ERR_NOT_IN_RANGE) {
-                              creep.moveTo(creep.room.controller);
-                          }
-                      }
-                    }
-                }
-                break;
-            case 1:
-                if (creep.room.name != "W49S28") {
-                    creep.moveTo(new RoomPosition(25, 25, "W49S28"));
-                } else if (creep.room.name == "W49S28") {
-                    if (!creep.room.controller.my) {
-                        if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(creep.room.controller);
-                        }
-                    }
-                }
-                break;
-            case 2:
-                if (creep.room.name != "W49S28") {
-                    creep.moveTo(new RoomPosition(25, 25, "W49S28"));
-                } else if (creep.room.name == "W49S28") {
-                    if (!creep.room.controller.my) {
-                        if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(creep.room.controller);
-                        }
-                    }
-                }
-
-                break;
-        }
-
     }
 };
 module.exports = roleClaimer;
