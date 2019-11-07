f0d69f23d63bf119c98a0db516086f539de8211f
Igor Shaposhnikov <jourloy@yandex.ru>
2019-11-08 01:04:01 +0300

    Уменьшение потребление CPU

--- role.transporter.js
+++ role.transporter.js
@@ -1,63 +1,37 @@
 var roleTransporter = {
     /** @param {Creep} creep **/
     control(creep) {
-        var hostileCreeps = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        var targets = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
-            }
-        });
-
-        var targets2 = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER);
-            }
-        });
-        var tower = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_TOWER) && structure.energy < 700;
-            }
-        });
-        var rampart = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_RAMPART);
-            }
-        });
-        var road = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_ROAD);
-            }
-        });
-        const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES);
-        var storageInRoom = creep.room.storage;
-        var constructionSite = creep.room.find(FIND_CONSTRUCTION_SITES);
-        if (creep.spawning) {
-            creep.memory.room = creep.room.name;
+      var targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_EXTENSION ||
+            structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+          }
+      });
+      var tower = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_TOWER) && structure.energy < 700;
         }
+      });
+      const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES);
+      var storageInRoom = creep.room.storage;
+
+      if (creep.spawning) {
+          creep.memory.room = creep.room.name;
+      }
 
         if (creep.memory.room == creep.room.name) {
-            if (Memory.mode == 1) {
-                if (creep.store[RESOURCE_ENERGY] === 0) {
-                    creep.memory.work = 0;
-                } else if (targets.length > 0 || targets2.length > 0) {
-                    creep.memory.work = 2;
-                    //} else if (road && road.hits < road.hitsMax) {
-                    //    creep.memory.work = 3;
-                } else if (tower[0] || tower[1]) {
-                    creep.memory.work = 4;
-                } else if (storageInRoom) {
-                    creep.memory.work = 5;
-                } else if (constructionSite) {
-                    creep.memory.work = 6;
-                }
-            } else if (Memory.mode == 2) {
-                if (creep.store[RESOURCE_ENERGY] === 0) {
-                    creep.memory.work = 0;
-                } else if (creep.room.controller) {
-                    creep.memory.work = 6;
-                }
+            if (creep.store[RESOURCE_ENERGY] == 0) {
+              creep.memory.work = 0;
+            } else if (targets.length > 0) {
+              creep.memory.work = 2;
+            } else if (tower[0] || tower[1]) {
+              creep.memory.work = 4;
+            } else if (storageInRoom) {
+              creep.memory.work = 5;
+            } else {
+              creep.memory.work = 6;
             }
+
 
             switch (creep.memory.work) {
                 case 0:
@@ -95,43 +69,12 @@ var roleTransporter = {
                     }
                     break;
 
-                case 1:
-                    creep.say("🚓");
-                    if (creep.store[RESOURCE_ENERGY] != 0) {
-                        if (creep.transfer(tower[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(tower[0]);
-                        }
-                    } else {
-                        if (storageInRoom) {
-                            if (creep.withdraw(storageInRoom, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                                creep.moveTo(storageInRoom);
-                            }
-                        } else {
-                            if (creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
-                                creep.moveTo(droppedEnergy[0]);
-                            }
-                        }
-                    }
-                    break;
-
                 case 2:
                     creep.say("🚛");
-                    var energyStoreForSpawn = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
-                        }
-                    });
-                    if (creep.transfer(energyStoreForSpawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(energyStoreForSpawn);
+                    if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0]);
                     }
                     break;
-
-                case 3:
-                    creep.say("🛠️");
-                    if (creep.repair(road) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(road);
-                    }
 
                 case 4:
                     creep.say("🚚");
