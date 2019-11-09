b1353c04701775acc8bc7653e3bdb00f59a9f6d0
Igor Shaposhnikov <jourloy@yandex.ru>
2019-11-09 16:35:28 +0300

    Оптимизация

--- role.claimer.js
+++ role.claimer.js
@@ -12,17 +12,13 @@ var roleClaimer = {
           creep.moveTo(new RoomPosition(25, 25, targetRoom));
         } else if (creep.room.name == targetRoom) {
           if(creep.room.controller && !creep.room.controller.my) {
-            if(creep.attackController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(creep.room.controller);
-            }
-          } else {
             if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
               creep.moveTo(creep.room.controller);
             }
-            if(creep.signController(creep.room.controller, "Room under control") == ERR_NOT_IN_RANGE) {
+          }
+          if(creep.signController(creep.room.controller, "Room under control") == ERR_NOT_IN_RANGE) {
               creep.moveTo(creep.room.controller);
             }
-          }
         }
     }
 };
