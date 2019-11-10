35c131c5c818c3e6bb488df4ee6632fc456c68d5
Igor Shaposhnikov <jourloy@yandex.ru>
2019-11-11 00:37:45 +0300

    Изменено поведение

--- role.claimer.js
+++ role.claimer.js
@@ -5,21 +5,33 @@ var roleClaimer = {
             creep.memory.room = creep.room.name;
         }
 
-        var targetRoom = "W48S27"
-
+        targetRoom = "W49S26";
 
         if (creep.room.name != targetRoom) {
-          creep.moveTo(new RoomPosition(25, 25, targetRoom));
-        } else if (creep.room.name == targetRoom) {
-          if(creep.room.controller && !creep.room.controller.my) {
-            if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(creep.room.controller);
+          creep.moveTo(new RoomPosition(25, 25, targetRoom), {reusePath: 50});
+        } else {
+          if (creep.room.controller) {
+            if (!creep.room.controller.my) {
+
+              if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(creep.room.controller);
+              } else if (creep.claimController(creep.room.controller) == ERR_FULL) {
+                if(creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(creep.room.controller, {visualizePathStyle: {
+                    stroke: 'purple',
+                    lineStyle: 'dashed',
+                    opacity: 1,
+                  }, reusePath: 50});
+                }
+              }
+            } else {
+              if(creep.signController(creep.room.controller, "Room under control") == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(creep.room.controller);
+              }
             }
           }
-          if(creep.signController(creep.room.controller, "Room under control") == ERR_NOT_IN_RANGE) {
-              creep.moveTo(creep.room.controller);
-            }
         }
+
     }
 };
 module.exports = roleClaimer;
