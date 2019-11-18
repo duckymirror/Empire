842873f7c088af3c3dc2a724be35f6d6e7e55670
Igor Shaposhnikov <jourloy@yandex.ru>
2019-11-18 23:48:59 +0300

    Изменена логика

--- role.tower.js
+++ role.tower.js
@@ -9,32 +9,78 @@ var roleTower = {
             towers = towers.concat(roomTowers);
         }
         for (var i in towers){
+            
             var tower = towers[i];
-            var target = tower.room.find(FIND_HOSTILE_CREEPS);
-            if (target.length == 0) {
-                if (tower.energy < 700) {
-                    const targets = tower.room.find(FIND_STRUCTURES, {
-                        filter: (object) => object.hits < 300
-                    });
-
-                    targets.sort((a,b) => a.hits - b.hits);
-
-                    if(targets.length > 0) {
-                        tower.repair(targets[0]);
+            var hostileCreeps = tower.room.find(FIND_HOSTILE_CREEPS);
+            
+            var roads = tower.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_ROAD) && structure.hits < structure.hitsMax;
+                }
+            });
+            var walls = tower.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_WALL) && structure.hits < 1000000;
+                }
+            });
+            var allRamparts = tower.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_RAMPART);
+                }
+            });
+            if (allRamparts.length < 3) {
+                var ramparts = tower.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_RAMPART) && structure.hits < structure.hitsMax;
                     }
-                } else {
-                    const targets = tower.room.find(FIND_STRUCTURES, {
-                        filter: (object) => object.hits < object.hitsMax
-                    });
-
-                    targets.sort((a,b) => a.hits - b.hits);
-
-                    if(targets.length > 0) {
-                        tower.repair(targets[0]);
+                });
+            } else {
+                var ramparts = tower.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_RAMPART) && structure.hits < 1000000;
+                    }
+                });
+            }
+            var brokenRamparts = tower.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_RAMPART) && structure.hits < 10000;
+                }
+            });
+            
+            if (hostileCreeps.length == 0) {
+                if (tower.store[RESOURCE_ENERGY] > 699) {
+                    if (brokenRamparts.length > 0) {
+                        brokenRamparts.sort((a,b) => a.hits - b.hits);
+                        tower.repair(brokenRamparts[0]);
+                    } else if (roads.length > 0) {
+                        roads.sort((a,b) => a.hits - b.hits);
+                        tower.repair(roads[0]);
+                    } else if (ramparts.length > 0) {
+                        ramparts.sort((a,b) => a.hits - b.hits);
+                        tower.repair(ramparts[0]);
+                    } else if (walls.length > 0) {
+                        walls.sort((a,b) => a.hits - b.hits);
+                        tower.repair(walls[0]);
                     }
                 }
             } else {
-                tower.attack(target[0]);
+                if (hostileCreeps.length > 1) {
+                    if (hostileCreeps[0].owner.name = "kotyara") {
+                        if (hostileCreeps[1].owner.name = "kotyara") {
+                            console.log("Котяра здесь");
+                        } else {
+                            tower.attack(hostileCreeps[1]);
+                        }
+                    } else {
+                        tower.attack(hostileCreeps[1]);
+                    }
+                } else {
+                    if (hostileCreeps[0].owner.name = "kotyara") {
+                        console.log("Котяра здесь");
+                    } else {
+                        tower.attack(hostileCreeps[0]);
+                    }
+                }
             }
         }
     }
