72774ae1a2e1eb0a753eb45f48f4c40d341a533d
Igor Shaposhnikov <jourloy@yandex.ru>
2019-11-19 22:38:47 +0300

    Изменена логика

--- role.warrior.js
+++ role.warrior.js
@@ -5,12 +5,28 @@ var roleWarrior = {
             creep.memory.room = creep.room.name;
         }
 
-        var hostileCreeps = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3);
-        var hostileCreepsInRoom = creep.room.find(FIND_HOSTILE_CREEPS);
+        var hostileCreeps = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 3, {
+            filter: (creep) => {
+                return (creep.owner.username != "kotyara");
+            }
+        });
+        var hostileCreepsInRoom = creep.room.find(FIND_HOSTILE_CREEPS, {
+            filter: (creep) => {
+                return (creep.owner.username != "kotyara");
+            }
+        });
         if (hostileCreeps.length > 0) {
-            creep.rangedAttack(hostileCreeps[0]);
+            if(hostileCreeps.length > 1) {
+                creep.rangedAttack(hostileCreeps[1]);
+            } else {
+                creep.rangedAttack(hostileCreeps[0]);
+            }
         } else if (hostileCreepsInRoom.length > 0) {
-            creep.moveTo(hostileCreepsInRoom[0]);
+            if (hostileCreepsInRoom.length > 1) {
+                creep.moveTo(hostileCreepsInRoom[1]);
+            } else {
+                creep.moveTo(hostileCreepsInRoom[0]);
+            }
         } else {
             creep.moveTo(Game.flags.attack);
         }
