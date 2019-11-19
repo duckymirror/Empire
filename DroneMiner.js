42506f0a525e8f1262093c1ce888fa39a5a5f5b1
Igor Shaposhnikov <jourloy@yandex.ru>
2019-11-19 22:38:15 +0300

    Изменена логика + фикс

--- DroneMiner.js
+++ DroneMiner.js
@@ -30,7 +30,7 @@ var DroneMiner = {
             };
 
         } else {
-            var linkInRoom = creep.room.find(FIND_STRUCTURES, 1,{filter:s=>s.structureType == STRUCTURE_LINK});
+            var linkInRoom = creep.room.find(FIND_STRUCTURES,{filter:s=>s.structureType == STRUCTURE_LINK});
             var containerNear = creep.pos.findInRange(FIND_STRUCTURES, 1,{filter:s=>s.structureType == STRUCTURE_CONTAINER});
             var source = Game.getObjectById(creep.memory.sourceId);
 
@@ -39,6 +39,7 @@ var DroneMiner = {
                     creep.repair(containerNear[0])
                 }
             }
+
             if (creep.room.storage) {
 
                 if (linkInRoom.length > 0) {
@@ -66,43 +67,12 @@ var DroneMiner = {
 
                     } else {
 
-                        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(source, {reusePath: 50, visualizePathStyle: visualizePath});
-                        } else if (creep.harvest(source) == ERR_NOT_OWNER) {
-                            creep.say("1")
-                        } else if (creep.harvest(source) == ERR_NOT_ENOUGH_RESOURCES) {
-                            creep.say('2')
-                        } else {
-                            creep.harvest(source);
-                            creep.say("99")
-                        }
-                        creep.say("3");
-
-                    }
-
-                } else {
-
-                    if (creep.room.storage.store[RESOURCE_ENERGY] < 750001) {
-
-                        if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(source, {reusePath: 50, visualizePathStyle: visualizePath});
-                        } else if (creep.harvest(source) == ERR_NOT_OWNER) {
-                            creep.say("1")
-                        } else if (creep.harvest(source) == ERR_NOT_ENOUGH_RESOURCES) {
-                            creep.say('2')
-                        } else {
-                            creep.harvest(source);
-                            creep.say("99")
-                        }
-                        
-                    } else {
-
-                        if (containerNear.length == 1) {
-                            if (containerNear[0].store[RESOURCE_ENERGY] != containerNear[0].store.getCapacity) {
-                                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                                    creep.moveTo(containerNear[0].pos, {reusePath: 50, visualizePathStyle: visualizePath});
-                                    creep.say("98")
-                                } else if (creep.harvest(source) == ERR_NOT_OWNER) {
+                        if (containerNear.length == 1 && creep.pos.isNearTo(source)) {
+                            if (!creep.pos.isEqualTo(containerNear[0].pos)) {
+                                creep.moveTo(containerNear[0].pos, {reusePath: 50, visualizePathStyle: visualizePath});
+                                creep.say("98")
+                            } else if (containerNear[0].store[RESOURCE_ENERGY] < 2000) {
+                                if (creep.harvest(source) == ERR_NOT_OWNER) {
                                     creep.say("1")
                                 } else if (creep.harvest(source) == ERR_NOT_ENOUGH_RESOURCES) {
                                     creep.say('2')
@@ -111,7 +81,59 @@ var DroneMiner = {
                                     creep.say("99")
                                 }
                             } else {
-                                creep.say("01")
+                                creep.say("02")
+                            }
+                        }
+
+                    }
+
+                } else {
+
+                    if (creep.room.storage.store[RESOURCE_ENERGY] < 750001) {
+
+                        if (containerNear.length == 1 && creep.pos.isNearTo(source)) {
+                            if (!creep.pos.isEqualTo(containerNear[0].pos)) {
+                                creep.moveTo(containerNear[0].pos, {reusePath: 50, visualizePathStyle: visualizePath});
+                                creep.say("98")
+                            } else if (containerNear[0].store[RESOURCE_ENERGY] < 2000) {
+                                if (creep.harvest(source) == ERR_NOT_OWNER) {
+                                    creep.say("1")
+                                } else if (creep.harvest(source) == ERR_NOT_ENOUGH_RESOURCES) {
+                                    creep.say('2')
+                                } else {
+                                    creep.harvest(source);
+                                    creep.say("99")
+                                }
+                            } else {
+                                creep.say("02")
+                            }
+                        } else {
+                            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(source, {reusePath: 50, visualizePathStyle: visualizePath});
+                                creep.say("98")
+                            } else {
+                                creep.harvest(source);
+                                creep.say("99")
+                            }
+                        }
+                        
+                    } else {
+
+                        if (containerNear.length == 1 && creep.pos.isNearTo(source)) {
+                            if (!creep.pos.isEqualTo(containerNear[0].pos)) {
+                                creep.moveTo(containerNear[0].pos, {reusePath: 50, visualizePathStyle: visualizePath});
+                                creep.say("98")
+                            } else if (containerNear[0].store[RESOURCE_ENERGY] < 2000) {
+                                if (creep.harvest(source) == ERR_NOT_OWNER) {
+                                    creep.say("1")
+                                } else if (creep.harvest(source) == ERR_NOT_ENOUGH_RESOURCES) {
+                                    creep.say('2')
+                                } else {
+                                    creep.harvest(source);
+                                    creep.say("99")
+                                }
+                            } else {
+                                creep.say("02")
                             }
                         } else {
                             if (creep.pos.findInRange(source, 1)) {
