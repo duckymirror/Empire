41fa6b70215b5c3e871163823fc961222100fcf3
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-04 19:08:04 +0300

    Изменена логика

--- main.js
+++ main.js
@@ -1,27 +1,70 @@
+/*
+ ______________________________________
+/                                      \
+|                                      |
+|    ██  ██    ██    ██  ██    ████    |
+|    ██  ██    ██    ██  ██    ██      |
+|    ██████    ██    ██  ██    ████    |
+|    ██  ██    ██    ██  ██    ██      |
+|    ██  ██    ██      ██      ████    |
+|                                      |
+\______________SCREEPS AI______________/
+
+@ Author: Jourloy
+@ Repository: https://github.com/Jourloy/HIVE
+@ How to use: Will be soon
+
+*/
+
+let Console = require("Console");
+let ControlMemory = require("ControlMemory");
+let ControlRoom = require("ControlRoom");
 var roleSpawn = require("role.spawn");
 var roleTower = require("role.tower");
-var amountCreeps = require("amountCreeps");
-var amountCreepsIsLive = require("amountCreepsIsLive");
-
-Memory.target = {};
 
 Memory.room = {};
-Memory.amountCreeps = {};
 
-Memory.room.E45N9 = {};
-Memory.room.E46N9 = {};
-Memory.room.E48N5 = {};
-Memory.room.W49S26 = {};
+function stats () {
+    if (Game.spawns['SP-R1']) {
+        Memory.room.One.Name = Game.spawns['SP-R1'].room.name;
+        Memory.room.One.Controller.level = Game.spawns['SP-R1'].room.controller.level;
+        Memory.room.One.Controller.progress = Math.round(Game.spawns['SP-R1'].room.controller.progress / Game.spawns['SP-R1'].room.controller.progressTotal * 100);
+    }
+    if (Game.spawns['RT-SP2']) {
+        Memory.room.Two.Name = Game.spawns['RT-SP2'].room.name;
+        Memory.room.Two.Controller.level = Game.spawns['RT-SP2'].room.controller.level;
+        Memory.room.Two.Controller.progress = Math.round(Game.spawns['RT-SP2'].room.controller.progress / Game.spawns['RT-SP2'].room.controller.progressTotal * 100);
+    }
+    if (Game.spawns['SP-R3']) {
+        Memory.room.Three.Name = Game.spawns['SP-R3'].room.name;
+        Memory.room.Three.Controller.level = Game.spawns['SP-R3'].room.controller.level;
+        Memory.room.Three.Controller.progress = Math.round(Game.spawns['SP-R3'].room.controller.progress / Game.spawns['SP-R3'].room.controller.progressTotal * 100);
+    }
+    if (Game.spawns['SP-R']) {
+        Memory.room.Four.Name = Game.spawns['SP-R'].room.name;
+        Memory.room.Four.Controller.level = Game.spawns['SP-R'].room.controller.level;
+        Memory.room.Four.Controller.progress = Math.round(Game.spawns['SP-R'].room.controller.progress / Game.spawns['SP-R'].room.controller.progressTotal * 100);
+    }
+    if (Game.spawns['SP-R5']) {
+        Memory.room.Five.Name = Game.spawns['SP-R5'].room.name;
+        Memory.room.Five.Controller.level = Game.spawns['SP-R5'].room.controller.level;
+        Memory.room.Five.Controller.progress = Math.round(Game.spawns['SP-R5'].room.controller.progress / Game.spawns['SP-R5'].room.controller.progressTotal * 100);
+    }
+    
+}
 
 module.exports.loop = function () {
 
-    for (var name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-            delete Memory.creeps[name];
-        }
-    }
+    Memory.room.claim = "E47N3";
+
+
+    ControlMemory.setting();
+    stats();
+    ControlRoom.control();
+
+    Console.setting();
+    
     roleTower.control();
     roleSpawn.run();
-    amountCreepsIsLive.run();
-    amountCreeps.run();
+
 };
