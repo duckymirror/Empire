8232e38ffaedc9239f00501bbff3b5683f4c099e
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-06 17:21:48 +0300

    Добавлена новая роль

--- ControlMemory.js
+++ ControlMemory.js
@@ -56,16 +56,17 @@ function amountCreeps () {
     if (Game.spawns["SP-R1"]) {
         Memory.room.One.Creeps.Amount.Miners0             = 1; //Default: 1
         Memory.room.One.Creeps.Amount.Miners1             = 1; //Default: 1
-        Memory.room.One.Creeps.Amount.DroneTransporters   = 1; //Default: 2
-        Memory.room.One.Creeps.Amount.Builders            = 2; //Default: 2
+        Memory.room.One.Creeps.Amount.Drone               = 1; //Default: 1
+        Memory.room.One.Creeps.Amount.DroneTransporters   = 0; //Default: 2
+        Memory.room.One.Creeps.Amount.Builders            = 0; //Default: 2
         if (Game.flags.claim) {
             Memory.room.One.Creeps.Amount.Claimers        = 0; //Default: 1
             Memory.room.One.Creeps.Amount.FarBuilders     = 2; //Default: 2
         } else {
             Memory.room.One.Creeps.Amount.Claimers        = 0; //Default: 0
-            Memory.room.One.Creeps.Amount.FarBuilders     = 2; //Default: 0
+            Memory.room.One.Creeps.Amount.FarBuilders     = 0; //Default: 0
         }
-        Memory.room.One.Creeps.Amount.Refillers           = 1; //Default: 1
+        Memory.room.One.Creeps.Amount.Refillers           = 0; //Default: 1
         if (Game.flags.attack) {
             Memory.room.One.Creeps.Amount.zerglings       = 0; //Default: 1
             Memory.room.One.Creeps.Amount.Healers         = 0; //Default: 1
