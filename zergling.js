3ad09ab909e150afe35840c468bbe356d506429f
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-08 16:13:09 +0300

    Изменение логики + фикс

--- zergling.js
+++ zergling.js
@@ -2,12 +2,14 @@ var zergling = {
     /** @param {Creep} creep **/
     control(creep) {
         if (creep.spawning) {
+
             creep.memory.room = creep.room.name;
+            
         } else {
 
             var hostileCreeps = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS, {
                 filter: (creep) => {
-                    return (creep.owner.username != "kotyara" && creep.owner.username != "Kasami" && creep.owner.username != "Fostenel");
+                    return (creep.owner.username != "kotyara");
                 }
             });
             
@@ -16,47 +18,59 @@ var zergling = {
                     return (structure.structureType == STRUCTURE_TOWER ||
                             structure.structureType == STRUCTURE_SPAWN ||
                             structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_STORAGE) && structure.owner.username != "Kotyara" && structure.owner.username != "Kasami";
+                            structure.structureType == STRUCTURE_STORAGE || 
+                            structure.structureType == STRUCTURE_RAMPART) && structure.owner.username != "Kotyara";
+                }
+            });
+            var dangerHostileStructures = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_TOWER) && structure.owner.username != "Kotyara";
                 }
             });
 
-            if (hostileStructures) {
-
+            if (dangerHostileStructures) {
                 speak = ['FOR', 'THE', 'HIVE'];
                 speakNow = speak[Game.time % speak.length];
-                creep.say(speakNow);
+                creep.say(speakNow, true);
 
                 if (creep.attack(hostileStructures) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(hostileStructures, {heuristicWeight: 1.2, range: 1, reusePath: 10});
+                    creep.moveTo(hostileStructures, {heuristicWeight: 1.2, reusePath: 10});
                 }
-            } else if (hostileCreeps) {
+            } else if (hostileCreeps && creep.room.name == creep.memory.flagPos.room.name) {
                 
                 speak = ['FOR', 'THE', 'HIVE'];
                 speakNow = speak[Game.time % speak.length];
-                creep.say(speakNow);
+                creep.say(speakNow, true);
 
                 var hostileAttackCreeps = creep.room.find(FIND_HOSTILE_CREEPS, {
                     filter: (creep) => {
-                        return (creep.owner.username != "kotyara" && creep.owner.username != "Kasami") && (creep.body.type == "ATTACK" || creep.body.type == "RANGED_ATTACK");
+                        return (creep.owner.username != "kotyara") && (creep.body.type == "ATTACK" || creep.body.type == "RANGED_ATTACK");
                     }
                 });
 
                 if (hostileAttackCreeps.length > 0) {
                     if (creep.attack(hostileAttackCreeps[0]) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(hostileAttackCreeps[0], {heuristicWeight: 1.2, range: 1, reusePath: 10});
+                        creep.moveTo(hostileAttackCreeps[0]);
                     }
                 } else {
                     if (creep.attack(hostileCreeps) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(hostileCreeps, {heuristicWeight: 1.2, range: 1, reusePath: 10});
+                        creep.moveTo(hostileCreeps);
                     }
                 }
+            } else if (hostileStructures) {
+
+                speak = ['FOR', 'THE', 'HIVE'];
+                speakNow = speak[Game.time % speak.length];
+                creep.say(speakNow, true);
+
+                if (creep.attack(hostileStructures) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(hostileStructures, {heuristicWeight: 1.2, reusePath: 10});
+                }
             } else {
-                if (Game.flags.attack) {
-                    creep.moveTo(Game.flags.attack, {heuristicWeight: 1.2, range: 1, reusePath: 50});
+                if (creep.memory.flagPos.name == Game.flags.attack1.name) {
+                    creep.moveTo(Game.flags.attack1, {heuristicWeight: 1.2, range: 1, reusePath: 10});
                 } else {
-                    if(creep.signController(creep.room.controller, "HIVE") == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(creep.room.controller);
-                    }
+                    creep.moveTo(Game.flags.attack2, {heuristicWeight: 1.2, range: 1, reusePath: 10});
                 }
             }
 
