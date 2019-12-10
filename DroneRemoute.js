8f81cec0337e7386b5075cd2d8b59df9c561034b
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-10 20:09:16 +0300

    Фикс

--- DroneRemoute.js
+++ DroneRemoute.js
@@ -7,6 +7,10 @@ var DroneRemoute = {
             
         } else {
 
+            if (creep.ticksToLive < 100) {
+                Memory.room.One.Creeps.Amount.DroneRemoute--;
+            }
+
             if (creep.store.getUsedCapacity() == 0) {
                 creep.memory.work = 'getResource';
             } else if (creep.store.getUsedCapacity() == creep.store.getCapacity()) {
@@ -40,13 +44,36 @@ var DroneRemoute = {
                                 creep.moveTo(creep.room.storage, {ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50});
                             }
                         } else {
-                            if (creep.room.storage) {
-                                speak = ['OPEN', 'THE', 'GATES'];
+                            if (creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] > creep.store.getFreeCapacity()) {
+                                speak = ['FOR', 'THE', 'ZERG'];
                                 speakNow = speak[Game.time % speak.length];
                                 creep.say(speakNow, true);
 
-                                creep.moveTo(Game.flags.clearRoom, {heuristicWeight: 1.2, range: 1, reusePath: 50});
+                                var hostileStructures = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+                                    filter: (structure) => {
+                                        return ((structure.structureType == STRUCTURE_RAMPART) && structure.owner.username != "Kotyara") || ((structure.structureType == STRUCTURE_RAMPART) && structure.pos.isEqualTo(creep.room.storage.pos));
+                                    }
+                                });
+
+                                if(hostileStructures) {
+                                    if (creep.dismantle(hostileStructures) == ERR_NOT_IN_RANGE) {
+                                        creep.moveTo(hostileStructures, {heuristicWeight: 1.2, reusePath: 10});
+                                    }
+
+                                    let droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
+                                        filter: (droppedEnergy) => {
+                                            return droppedEnergy.pos.isEqualTo(creep.pos);
+                                        }
+                                    });
+
+                                    if (droppedEnergy) {
+                                        creep.pickup(droppedEnergy);
+                                    }
+                                }
+                                
                             } else {
+                                Memory.room.One.Creeps.Amount.zerglings = 2;
+                                Memory.room.One.Creeps.Amount.DroneRemoute = 0;
                                 creep.moveTo(Game.flags.clearRoom, {heuristicWeight: 1.2, range: 1, reusePath: 50});
                             }
                         }
@@ -64,8 +91,8 @@ var DroneRemoute = {
                             creep.moveTo(creep.room.storage, {ignoreRoads: true, euristicWeight: 1.2, range: 1, reusePath: 50});
                         }
                     } else {
-                        if (creep.transfer(creep.room.storage, RESOURCE_UTRIUM) == ERR_NOT_IN_RANGE) {
-                            creep.moveTo(creep.room.storage, {ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50});
+                        if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(creep.room.controller, {ignoreRoads: true, heuristicWeight: 1.2, range: 3, reusePath: 30});
                         }
                     }
                 }
