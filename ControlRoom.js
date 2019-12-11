f40231712f21bc51f893bb467a3eb6ac08879204
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-11 22:01:34 +0300

    Обновление

--- ControlRoom.js
+++ ControlRoom.js
@@ -6,37 +6,39 @@ function Visual() {
     const Y = -0.4;
 
     if (Game.spawns["Spawn1"]) {
-        let X = firstX + step * Memory.room.One.Controller.progress;
+        const room = Game.rooms[Memory.room.One.Name];
 
-        Game.rooms[Memory.room.One.Name].visual.rect(-0.35, -0.35, X, 0.8, { opacity: 0.3 })
+        let X = firstX + step * Memory.room.One.Stats.Controller.progress;
+        room.visual.rect(-0.35, -0.35, X, 0.8, { opacity: 0.3 })
+        room.visual.rect(-0.4, -0.4, 7.4, 8.9, { opacity: 0.1 }).poly([[-0.4, -0.4], [7, -0.4], [7, 8.5], [-0.4, 8.5], [-0.4, -0.4]]);
+        room.visual.text("Controller progress:", 2.4, 0.2, { color: 'white', font: 0.6 });
+        room.visual.text(Memory.room.One.Stats.Controller.progress + "%", 6, 0.2, { color: 'white', font: 0.6 });
+        room.visual.text("Made by JOURLOY", 3.4, 8.3, { color: 'red', font: 0.5 });
+        room.visual.line(-0.35, 0.5, 6.95, 0.5, { opacity: 0.5 })
 
-        Game.rooms[Memory.room.One.Name].visual.rect(-0.4, -0.4, 7.4, 8.9, { opacity: 0.1 }).poly([[-0.4, -0.4], [7, -0.4], [7, 8.5], [-0.4, 8.5], [-0.4, -0.4]]);
-        Game.rooms[Memory.room.One.Name].visual.text("Controller progress:", 2.4, 0.2, { color: 'white', font: 0.6 });
-        Game.rooms[Memory.room.One.Name].visual.text(Memory.room.One.Controller.progress + "%", 6, 0.2, { color: 'white', font: 0.6 });
-        Game.rooms[Memory.room.One.Name].visual.text("Made by JOURLOY", 3.4, 8.3, { color: 'red', font: 0.5 });
-        Game.rooms[Memory.room.One.Name].visual.line(-0.35, 0.5, 6.95, 0.5, { opacity: 0.5 })
+
     }
 
     if (Game.spawns["Spawn2"]) {
-        let X = firstX + step * Memory.room.Two.Controller.progress;
+        const room = Game.rooms[Memory.room.Two.Name];
 
-        Game.rooms[Memory.room.Two.Name].visual.rect(-0.35, -0.35, X, 0.8, { opacity: 0.3 })
-
-        Game.rooms[Memory.room.Two.Name].visual.rect(-0.4, -0.4, 7.4, 8.9, { opacity: 0.1 }).poly([[-0.4, -0.4], [7, -0.4], [7, 8.5], [-0.4, 8.5], [-0.4, -0.4]]);
-        Game.rooms[Memory.room.Two.Name].visual.text("Controller progress:", 2.4, 0.2, { color: 'white', font: 0.6 });
-        Game.rooms[Memory.room.Two.Name].visual.text(Memory.room.Two.Controller.progress + "%", 6, 0.2, { color: 'white', font: 0.6 });
-        Game.rooms[Memory.room.Two.Name].visual.text("Made by JOURLOY", 3.4, 8.3, { color: 'red', font: 0.5 });
-        Game.rooms[Memory.room.Two.Name].visual.line(-0.35, 0.5, 6.95, 0.5, { opacity: 0.5 });
+        let X = firstX + step * Memory.room.Two.Stats.Controller.progress;
+        room.visual.rect(-0.35, -0.35, X, 0.8, { opacity: 0.3 })
+        room.visual.rect(-0.4, -0.4, 7.4, 8.9, { opacity: 0.1 }).poly([[-0.4, -0.4], [7, -0.4], [7, 8.5], [-0.4, 8.5], [-0.4, -0.4]]);
+        room.visual.text("Controller progress:", 2.4, 0.2, { color: 'white', font: 0.6 });
+        room.visual.text(Memory.room.Two.Stats.Controller.progress + "%", 6, 0.2, { color: 'white', font: 0.6 });
+        room.visual.text("Made by JOURLOY", 3.4, 8.3, { color: 'red', font: 0.5 });
+        room.visual.line(-0.35, 0.5, 6.95, 0.5, { opacity: 0.5 });
     }
 
     if (Game.spawns["SP-R3"]) {
-        let X = firstX + step * Memory.room.Three.Controller.progress;
+        let X = firstX + step * Memory.room.Three.Stats.Controller.progress;
 
         Game.rooms[Memory.room.Three.Name].visual.rect(-0.35, -0.35, X, 0.8, { opacity: 0.3 })
 
         Game.rooms[Memory.room.Three.Name].visual.rect(-0.4, -0.4, 7.4, 8.9, { opacity: 0.1 }).poly([[-0.4, -0.4], [7, -0.4], [7, 8.5], [-0.4, 8.5], [-0.4, -0.4]]);
         Game.rooms[Memory.room.Three.Name].visual.text("Controller progress:", 2.4, 0.2, { color: 'white', font: 0.6 });
-        Game.rooms[Memory.room.Three.Name].visual.text(Memory.room.Three.Controller.progress + "%", 6, 0.2, { color: 'white', font: 0.6 });
+        Game.rooms[Memory.room.Three.Name].visual.text(Memory.room.Three.Stats.Controller.progress + "%", 6, 0.2, { color: 'white', font: 0.6 });
         Game.rooms[Memory.room.Three.Name].visual.text("Made by JOURLOY", 3.4, 8.3, { color: 'red', font: 0.5 });
         Game.rooms[Memory.room.Three.Name].visual.line(-0.35, 0.5, 6.95, 0.5, { opacity: 0.5 });
     }
@@ -49,8 +51,6 @@ function amountCreepsIsLive() {
     const DroneMiner = require("DroneMiner");
     const Drone = require("Drone");
     const DroneRemoute = require("DroneRemoute");
-    const roleClaimer = require("role.claimer");
-    const roleFarBuilder = require("role.farBuilder");
     const zergling = require("zergling");
     const DroneHealer = require("DroneHealer");
 
@@ -59,8 +59,6 @@ function amountCreepsIsLive() {
         Memory.room.One.Creeps.AmountIsLive.Miners1 = 0;
         Memory.room.One.Creeps.AmountIsLive.Drone = 0;
         Memory.room.One.Creeps.AmountIsLive.DroneRemoute = 0;
-        Memory.room.One.Creeps.AmountIsLive.Claimers = 0;
-        Memory.room.One.Creeps.AmountIsLive.FarBuilders = 0;
         Memory.room.One.Creeps.AmountIsLive.Healers = 0;
         Memory.room.One.Creeps.AmountIsLive.zerglings = 0;
         Memory.room.One.Creeps.AmountIsLive.Overlords = 0;
@@ -70,8 +68,6 @@ function amountCreepsIsLive() {
         Memory.room.Two.Creeps.AmountIsLive.Miners0 = 0;
         Memory.room.Two.Creeps.AmountIsLive.Miners1 = 0;
         Memory.room.Two.Creeps.AmountIsLive.Drone = 0;
-        Memory.room.Two.Creeps.AmountIsLive.ClaimersIsLive = 0;
-        Memory.room.Two.Creeps.AmountIsLive.FarBuilders = 0;
         Memory.room.Two.Creeps.AmountIsLive.Healers = 0;
         Memory.room.Two.Creeps.AmountIsLive.zerglings = 0;
     }
@@ -80,8 +76,6 @@ function amountCreepsIsLive() {
         Memory.room.Three.Creeps.AmountIsLive.Miners0 = 0;
         Memory.room.Three.Creeps.AmountIsLive.Miners1 = 0;
         Memory.room.Three.Creeps.AmountIsLive.Drone = 0;
-        Memory.room.Three.Creeps.AmountIsLive.Claimers = 0;
-        Memory.room.Three.Creeps.AmountIsLive.FarBuilders = 0;
         Memory.room.Three.Creeps.AmountIsLive.Healers = 0;
         Memory.room.Three.Creeps.AmountIsLive.zerglings = 0;
     }
@@ -90,8 +84,6 @@ function amountCreepsIsLive() {
         Memory.room.Four.Creeps.AmountIsLive.Miners0 = 0;
         Memory.room.Four.Creeps.AmountIsLive.Miners1 = 0;
         Memory.room.Four.Creeps.AmountIsLive.Drone = 0;
-        Memory.room.Four.Creeps.AmountIsLive.Claimers = 0;
-        Memory.room.Four.Creeps.AmountIsLive.FarBuilders = 0;
         Memory.room.Four.Creeps.AmountIsLive.Healers = 0;
         Memory.room.Four.Creeps.AmountIsLive.zerglings = 0;
     }
@@ -100,8 +92,6 @@ function amountCreepsIsLive() {
         Memory.room.Five.Creeps.AmountIsLive.Miners0 = 0;
         Memory.room.Five.Creeps.AmountIsLive.Miners1 = 0;
         Memory.room.Five.Creeps.AmountIsLive.Drone = 0;
-        Memory.room.Five.Creeps.AmountIsLive.Claimers = 0;
-        Memory.room.Five.Creeps.AmountIsLive.FarBuilders = 0;
         Memory.room.Five.Creeps.AmountIsLive.Healers = 0;
     }
 
@@ -163,34 +153,6 @@ function amountCreepsIsLive() {
                     Memory.room.Six.Creeps.AmountIsLive.Drone++;
                 }
                 break;
-            case "claimer":
-                roleClaimer.control(creep);
-                if (Game.spawns['Spawn1'] && creep.memory.room == Memory.room.One.Name) {
-                    Memory.room.One.Creeps.AmountIsLive.Claimers++;
-                } else if (Game.spawns['Spawn2'] && creep.memory.room == Memory.room.Two.Name) {
-                    Memory.room.Two.Creeps.AmountIsLive.Claimers++;
-                } else if (Game.spawns['SP-R3'] && creep.memory.room == Memory.room.Three.Name) {
-                    Memory.room.Three.Creeps.AmountIsLive.Claimers++;
-                } else if (Game.spawns['SP-R'] && creep.memory.room == Memory.room.Four.Name) {
-                    Memory.room.Four.Creeps.AmountIsLive.Claimers++;
-                } else if (Game.spawns['SP-R5'] && creep.memory.room == Memory.room.Five.Name) {
-                    Memory.room.Five.Creeps.AmountIsLive.Claimers++;
-                }
-                break;
-            case "FarBuilder":
-                roleFarBuilder.control(creep);
-                if (Game.spawns['Spawn1'] && creep.memory.room == Memory.room.One.Name) {
-                    Memory.room.One.Creeps.AmountIsLive.FarBuilders++;
-                } else if (Game.spawns['Spawn2'] && creep.memory.room == Memory.room.Two.Name) {
-                    Memory.room.Two.Creeps.AmountIsLive.FarBuilders++;
-                } else if (Game.spawns['SP-R3'] && creep.memory.room == Memory.room.Three.Name) {
-                    Memory.room.Three.Creeps.AmountIsLive.FarBuilders++;
-                } else if (Game.spawns['SP-R'] && creep.memory.room == Memory.room.Four.Name) {
-                    Memory.room.Four.Creeps.AmountIsLive.FarBuilders0IsLive++;
-                } else if (Game.spawns['SP-R5'] && creep.memory.room == Memory.room.Five.Name) {
-                    Memory.room.Five.Creeps.AmountIsLive.FarBuilders0IsLive++;
-                }
-                break;
             case "healer":
                 DroneHealer.control(creep);
                 if (Game.spawns['SP-R3'] && creep.memory.room == Memory.room.Three.Name) {
@@ -218,11 +180,13 @@ function amountCreepsIsLive() {
                 if (Game.spawns['Spawn1'] && creep.memory.room == Memory.room.One.Name) {
                     Memory.room.One.Creeps.AmountIsLive.Overlords++;
                 }
+                break;
             case "DroneRemoute":
                 DroneRemoute.control(creep);
                 if (Game.spawns['Spawn1'] && creep.memory.room == Memory.room.One.Name) {
                     Memory.room.One.Creeps.AmountIsLive.DroneRemoute++;
                 }
+                break;
 
         }
     }
