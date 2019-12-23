83e5d1552c0d7df9577cc51f4b9a164900ec8829
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-23 18:26:00 +0300

    Ребаланс

--- ControlMemory.js
+++ ControlMemory.js
@@ -3,45 +3,42 @@ function amountCreeps() {
         let room = Game.rooms[z];
 
         for (let i in Memory.rolies) {
-            if ("Drone" == Memory.rolies[i]){
-                if (room.find(FIND_SOURCES_ACTIVE).length > 1) {
-                    switch (room.controller.level) {
-                        case 1:
+            if ("Drone" == Memory.rolies[i]) {
+                switch (room.controller && room.controller.my && room.controller.level) {
+                    case 1:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
+                        break;
+                    case 2:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
+                        break;
+                    case 3:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
+                        break;
+                    case 4:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
+                        break;
+                    case 5:
+                        let Nydus = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_LINK } });
+                        if (Nydus.length > 1) {
                             Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
-                            break;
-                        case 2:
+                        } else {
                             Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
-                            break;
-                        case 3:
-                            Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
-                            break;
-                        case 4:
-                            Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
-                            break;
-                        case 5:
-                            let Nydus = room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_LINK}});
-                            if (Nydus.length > 1) {
-                                Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
-                            } else {
-                                Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
-                            }
-                            break;
-                        case 6:
-                            Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
-                            break;
-                        case 6:
-                            Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
-                            break;
-                        case 7:
-                            Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
-                            break;
-                        case 8:
-                            Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
-                            break;
-                    }
-                } else {
-                    Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
+                        }
+                        break;
+                    case 6:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
+                        break;
+                    case 6:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
+                        break;
+                    case 7:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
+                        break;
+                    case 8:
+                        Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
+                        break;
                 }
+
             }
 
             if ("DroneMiner" == Memory.rolies[i]) {
@@ -51,6 +48,8 @@ function amountCreeps() {
             if ("Zergling" == Memory.rolies[i]) {
                 if (Game.flags.attack) {
                     Memory.room[room.name + ".amount." + Memory.rolies[i]] = 0;
+                } else if (Game.flags.power) {
+                    Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
                 } else {
                     Memory.room[room.name + ".amount." + Memory.rolies[i]] = 0;
                 }
@@ -66,14 +65,14 @@ function amountCreeps() {
 
             if ("DroneRemoute" == Memory.rolies[i]) {
                 if (Game.flags.claim) {
-                    Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
+                    Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
                 } else {
                     Memory.room[room.name + ".amount." + Memory.rolies[i]] = 0;
                 }
             }
 
         }
-        
+
     }
 }
 
