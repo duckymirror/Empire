1ab2629fdabcd57f37a01f4d15877f563bb52eb8
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-31 20:41:46 +0300

    Фикс

--- DroneControl.js
+++ DroneControl.js
@@ -133,8 +133,7 @@ function doUpgrade(creep) {
                 structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
         }
     });
-
-    if (spawnEnergy.length > 0) doRefill(creep)
+    if (spawnEnergy.length > 0 && Memory.room[creep.room.name + ".amountIsLive." + "DroneRefiller"] == 0 || (Memory.room[creep.room.name + ".amountIsLive." + "DroneUpgrader"] == 0 && creep.memory.role == "DroneBuilder")) doRefill(creep)
     else if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) creep.moveTo(creep.room.controller, { heuristicWeight: 1.2, range: 3, reusePath: 50 });
 }
 
