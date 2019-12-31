65e342860ced00a4d4cd37d256f754526449d0ef
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-31 19:45:59 +0300

    Фикс

--- DroneControl.js
+++ DroneControl.js
@@ -12,7 +12,7 @@ function getResource(creep) {
             } else {
                 const containerInRoom = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0;
+                        return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] >= creep.store.getFreeCapacity();
                     }
                 });
                 if (containerInRoom.length == 1) {
@@ -37,7 +37,7 @@ function getResource(creep) {
         } else if (nydus.length < 2) {
             const containerInRoom = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0;
+                    return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] >= creep.store.getFreeCapacity();
                 }
             });
             if (containerInRoom.length == 1) {
@@ -58,6 +58,34 @@ function getResource(creep) {
                     if (creep.harvest(source) == ERR_NOT_IN_RANGE) creep.moveTo(source);
                 }
             }
+        }
+    }
+}
+
+function doRefill(creep) {
+    const spawnEnergy = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (structure) => {
+            return (structure.structureType == STRUCTURE_EXTENSION ||
+                structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+        }
+    });
+    if (spawnEnergy) {
+        if (creep.transfer(spawnEnergy, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(spawnEnergy, { heuristicWeight: 1.2, range: 1, reusePath: 50 });
+        }
+    } else if (creep.memory.role == "DroneRefiller") {
+        const towerWithoutEnergy = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_TOWER) && structure.store[RESOURCE_ENERGY] <= 750;
+            }
+        });
+        if (towerWithoutEnergy.length > 0 && creep.room.controller.level > 2) {
+            towerWithoutEnergy.sort((a, b) => a.store[RESOURCE_ENERGY] - b.store[RESOURCE_ENERGY]);
+            if (creep.transfer(towerWithoutEnergy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(towerWithoutEnergy[0], { heuristicWeight: 1.2, range: 1, reusePath: 50 });
+            }
+        } else {
+            doUpgrade(creep);
         }
     }
 }
@@ -111,21 +139,20 @@ function doUpgrade(creep) {
 }
 
 function doMine(creep) {
-    if (!Memory.room[creep.room.name + ".amountIsLive." + "DroneBuilder"] || Memory.room[creep.room.name + ".amountIsLive." + "DroneBuilder"] && Memory.room[creep.room.name + ".amountIsLive." + "DroneBuilder"] == 0) doRefill(creep);
     const linkInRoom = creep.room.find(FIND_STRUCTURES, { filter: s => s.structureType == STRUCTURE_LINK });
     if (linkInRoom.length > 0) {
         var linkIsNear = creep.pos.findInRange(FIND_STRUCTURES, 1, { filter: s => s.structureType == STRUCTURE_LINK });
     }
     const containerNear = creep.pos.findInRange(FIND_STRUCTURES, 1, { filter: s => s.structureType == STRUCTURE_CONTAINER });
     const sourceInRoom = creep.room.find(FIND_SOURCES);
-    
+
     if (creep.memory.role == "DroneMiner1" && (!creep.memory.source || creep.memory.source == null)) {
         creep.memory.source = sourceInRoom[0].id;
-    } else if (creep.memory.role == "DroneMiner2" && sourceInRoom.length == 2 && (!creep.memory.source|| creep.memory.source == null)) {
+    } else if (creep.memory.role == "DroneMiner2" && sourceInRoom.length == 2 && (!creep.memory.source || creep.memory.source == null)) {
         creep.memory.source = sourceInRoom[1].id;
     }
 
-    const source =  Game.getObjectById(creep.memory.source);
+    const source = Game.getObjectById(creep.memory.source);
 
     if (creep.store[RESOURCE_ENERGY] == 0) creep.memory.repair = false;
     else if (creep.store[RESOURCE_ENERGY] == creep.store.getCapacity()) creep.memory.repair = true;
@@ -148,7 +175,7 @@ function doMine(creep) {
                     creep.harvest(source);
                 }
             } else {
-                creep.moveTo(source, { rignoreCreeps: false, reusePath: 50 });
+                creep.moveTo(source, { ignoreCreeps: false, reusePath: 50 });
             }
         } else {
             if (linkIsNear[0].store[RESOURCE_ENERGY] < 800) {
@@ -164,50 +191,42 @@ function doMine(creep) {
     } else {
         if (containerNear.length == 1 && creep.pos.isNearTo(source)) {
             if (!creep.pos.isEqualTo(containerNear[0].pos)) {
-                creep.moveTo(containerNear[0].pos, {ignoreCreeps: false, reusePath: 50 });
+                creep.moveTo(containerNear[0].pos, { ignoreCreeps: false, reusePath: 50 });
             } else if (containerNear[0].store[RESOURCE_ENERGY] < 1950) {
                 creep.harvest(source);
             }
         } else {
-            creep.moveTo(source, {ignoreCreeps: false, reusePath: 10 });
+            creep.moveTo(source, { ignoreCreeps: false, reusePath: 10 });
         }
-    }
-}
-
-function doRefill(creep) {
-    const spawnEnergy = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: (structure) => {
-            return (structure.structureType == STRUCTURE_EXTENSION ||
-                structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
-        }
-    });
-    if (creep.transfer(spawnEnergy, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(spawnEnergy, { heuristicWeight: 1.2, range: 1, reusePath: 50 });
     }
 }
 
 function doWork(creep) {
     switch (creep.memory.role) {
         case "DroneBuilder":
+            creep.say("Builder")
             const constructionSiteInRoom = creep.room.find(FIND_CONSTRUCTION_SITES);
             if (constructionSiteInRoom.length > 0) doBuild(creep, constructionSiteInRoom.length);
             else doUpgrade(creep);
             break;
         case "DroneMiner1":
-            creep.say("MINE")
+            creep.say("Miner")
             doMine(creep);
             break;
         case "DroneMiner2":
-            creep.say("MINE")
+            creep.say("Miner")
             doMine(creep);
             break;
         case "DroneRefiller":
+            creep.say("Refiller")
             doRefill(creep);
             break;
         case "DroneSeller":
+            creep.say("Seller")
             doSell(creep);
             break;
         case "DroneUpgrader":
+            creep.say("Upgrader")
             doUpgrade(creep);
             break;
     }
@@ -222,9 +241,11 @@ const DroneControl = {
             if (creep.room.name == creep.memory.room) {
                 if (creep.store.getUsedCapacity() == 0 && (creep.memory.role != "DroneMiner1" && creep.memory.role != "DroneMiner2")) creep.memory.state = "getResource";
                 else if (creep.store.getUsedCapacity() == creep.store.getCapacity() || (creep.memory.role == "DroneMiner1" || creep.memory.role == "DroneMiner2")) creep.memory.state = "doWork";
+                if (creep.memory.state == "getResource") getResource(creep);
+                if (creep.memory.state == "doWork") doWork(creep);
+            } else {
+                creep.moveTo(new RoomPosition(25, 25, creep.memory.room), { ignoreRoads: true, heuristicWeight: 1.2, range: 1, reusePath: 50 });
             }
-            if (creep.memory.state == "getResource") getResource(creep);
-            if (creep.memory.state == "doWork") doWork(creep);
         }
     }
 }
