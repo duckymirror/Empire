8eb03d633a2980eb5da8ac3ea7bf097417da8320
Igor Shaposhnikov <jourloy@yandex.ru>
2019-12-31 20:41:43 +0300

    Фикс

--- Control.js
+++ Control.js
@@ -7,40 +7,58 @@ function amountCreeps() {
     Memory.rolies = ["DroneBuilder", "DroneMiner1", "DroneMiner2", "DroneRefiller", "DroneSeller", "DroneUpgrader", "Zergling"];
     for (let z in Game.rooms) {
         let room = Game.rooms[z];
-        for (let i in Memory.rolies) {
-            if ("DroneBuilder" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]  = 2;
-            if ("DroneMiner1" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]   = 1;
-            if ("DroneMiner2" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]   = 1;
-            if ("DroneRefiller" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
-            if ("DroneSeller" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]   = 0;
-            if ("DroneUpgrader" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
-            if ("Zergling" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]      = 0;
+        if (room.controller && room.controller.my) {
+            for (let i in Memory.rolies) {
+                if ("DroneBuilder" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]  = 2;
+                if ("DroneMiner1" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]   = 1;
+                if ("DroneMiner2" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]   = 1;
+                if ("DroneRefiller" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]] = 1;
+                if ("DroneSeller" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]   = 0;
+                if ("DroneUpgrader" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]] = 2;
+                if ("Zergling" == Memory.rolies[i]) Memory.room[room.name + ".amount." + Memory.rolies[i]]      = 0;
+            }
         }
     }
 }
 function amountCreepsIsLive() {
-    const DroneControl = require("DroneControl");
     for (let z in Game.rooms) {
         let room = Game.rooms[z];
-        for (let i in Game.creeps) {
-            let creep = Game.creeps[i];
-            Memory.room[room.name + ".amountIsLive." + creep.memory.role] = 0
+        if (room.controller && room.controller.my) {
+            for (let i in Game.creeps) {
+                let creep = Game.creeps[i];
+                Memory.room[room.name + ".amountIsLive." + creep.memory.role] = 0
+            }
         }
     }
     for (let z in Game.rooms) {
         let room = Game.rooms[z];
-        for (let i in Game.creeps) {
-            let creep = Game.creeps[i];
-            if (room.name == creep.memory.room) Memory.room[room.name + ".amountIsLive." + creep.memory.role]++;
-            DroneControl.control(creep);
+        if (room.controller && room.controller.my) {
+            for (let i in Game.creeps) {
+                let creep = Game.creeps[i];
+                console.log(creep.memory.role + " " + Game.time%100)
+                if (room.name == creep.memory.room) Memory.room[room.name + ".amountIsLive." + creep.memory.role]++;
+            }
         }
     }
 }
 
+function runCreep() {
+    const DroneControl = require("DroneControl");
+    for (let z in Game.rooms) {
+        let room = Game.rooms[z];
+        if (room.controller && room.controller.my) {
+            for (let i in Game.creeps) {
+                let creep = Game.creeps[i];
+                DroneControl.control(creep);
+            }
+        }
+    }
+}
 const Control = {
     control(colony) {
         amountCreeps();
         amountCreepsIsLive();
+        runCreep();
     }
 }
 module.exports = Control;
