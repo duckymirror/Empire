126b48987827281057a981c89c75a13d15389fd2
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-02 19:19:11 +0300

    Фикс

--- main.js
+++ main.js
@@ -25,10 +25,28 @@ const Nydus = require("Nydus");
 Memory.room = {};
 
 module.exports.loop = function () {
-    
+
     Control.control()
     Console.setting();
     Nydus.run();
     roleTower.control();
-    roleSpawn.run();
+    let spawns = [];
+    for (let i in Game.rooms) {
+        let room = Game.rooms[i];
+        let roomSpawns = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
+        spawns = spawns.concat(roomSpawns);
+    }
+    for (let i in spawns) {
+        let spawn = spawns[i];
+        let amountEnergy = spawn.room.energyCapacityAvailable;
+
+        for (let i in Memory.rolies) {
+            console.log(Memory.rolies[i])
+            if (spawn.spawning == null) {
+                if ((!Memory.room[spawn.room.name + ".amountIsLive." + Memory.rolies[i]] && Memory.room[spawn.room.name + ".amount." + Memory.rolies[i]] > 0) || Memory.room[spawn.room.name + ".amountIsLive." + Memory.rolies[i]] < Memory.room[spawn.room.name + ".amount." + Memory.rolies[i]]) {
+                    roleSpawn.run(spawn, amountEnergy, Memory.rolies[i]);
+                }
+            }
+        }
+    }
 };
