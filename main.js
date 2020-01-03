602ed123d97c9e0f5c8003b7a3d290df0d8a4f54
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-03 19:19:34 +0300

    Рефакторинг

--- main.js
+++ main.js
@@ -29,13 +29,22 @@ module.exports.loop = function () {
     Console.setting();
     Nydus.run();
     roleTower.control();
-    let spawn = Game.spawns['Spawn1'];
-    let amountEnergy = spawn.room.energyCapacityAvailable;
 
-    for (let i in Memory.rolies) {
-        if (spawn.spawning == null) {
-            if ((!Memory.room[spawn.room.name + ".amountIsLive." + Memory.rolies[i]] && Memory.room[spawn.room.name + ".amount." + Memory.rolies[i]] > 0) || Memory.room[spawn.room.name + ".amountIsLive." + Memory.rolies[i]] < Memory.room[spawn.room.name + ".amount." + Memory.rolies[i]]) {
-                roleSpawn.run(spawn, amountEnergy, Memory.rolies[i]);
+    let spawns = [];
+    for (let i in Game.rooms) {
+        let room = Game.rooms[i];
+        let roomSpawns = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_SPAWN } });
+        spawns = spawns.concat(roomSpawns);
+    }
+    for (let i in spawns) {
+        let spawn = spawns[i];
+        let amountEnergy = spawn.room.energyCapacityAvailable;
+
+        for (let i in Memory.rolies) {
+            if (spawn.spawning == null) {
+                if ((!Memory.room[spawn.room.name + ".amountIsLive." + Memory.rolies[i]] && Memory.room[spawn.room.name + ".amount." + Memory.rolies[i]] > 0) || Memory.room[spawn.room.name + ".amountIsLive." + Memory.rolies[i]] < Memory.room[spawn.room.name + ".amount." + Memory.rolies[i]]) {
+                    roleSpawn.run(spawn, amountEnergy, Memory.rolies[i]);
+                }
             }
         }
     }
