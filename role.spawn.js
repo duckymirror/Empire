18c7a2631e201ed869e40c06ef3a201965885057
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-08 19:11:40 +0300

    Добавлен продавец

--- role.spawn.js
+++ role.spawn.js
@@ -234,101 +234,9 @@ function creepBody(energy, role) {
         }
     }
 
-    if (role == 'CorruptorMain' || role == 'Corruptor') {
-        return [MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK,ATTACK]
-    }
-
-    if (role == 'DroneRemouteHealMain' || role == 'DroneRemouteHeal') {
-        return [MOVE,MOVE,MOVE,MOVE,HEAL,HEAL,HEAL,HEAL]
-    }
-
-    if (role == 'Overseer') {
-        return [MOVE, MOVE, MOVE, MOVE, MOVE, CLAIM]
-    }
-
-    if (role == 'EliteStormtrooper') {
-        return [MOVE,MOVE,MOVE,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK,RANGED_ATTACK]
-    }
-
-    if (role == "EliteHeal") {
-        return [MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,HEAL,HEAL,HEAL,HEAL,HEAL,HEAL]
-    }
-
-    if (role == 'hydralisk') {
-        if (energy == 300) {
-            return [MOVE, RANGED_ATTACK]
-        } else if (energy == 350) {
-            return [MOVE, RANGED_ATTACK]
-        } else if (energy == 400) {
-            return [MOVE, RANGED_ATTACK]
-        } else if (energy == 450) {
-            return [MOVE, RANGED_ATTACK]
-        } else if (energy == 500) {
-            return [MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 550) {
-            return [MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 600) {
-            return [MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 650) {
-            return [MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 700) {
-            return [MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 750) {
-            return [MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 800) {
-            return [MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 850) {
-            return [MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 900) {
-            return [MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 950) {
-            return [MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        } else if (energy == 1000) {
-            return [MOVE, MOVE, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]
-        }
-    }
-
-    if (role == 'claimer') {
-        if (energy == 650) {
-            return [CLAIM, MOVE]
-        } else if (energy == 700) {
-            return [CLAIM, MOVE, MOVE]
-        } else if (energy == 750) {
-            return [CLAIM, MOVE, MOVE, MOVE]
-        } else if (energy == 800) {
-            return [CLAIM, MOVE, MOVE, MOVE, MOVE]
-        } else if (energy >= 850) {
-            return [CLAIM, MOVE, MOVE, MOVE, MOVE, MOVE]
-        }
-    }
-
-    if (role == "healer") {
-        if (energy >= 400 && energy < 1000) {
-            return [TOUGH, MOVE, MOVE, HEAL]
-        } else if (energy == 1000) {
-            return [MOVE, MOVE, MOVE, HEAL, HEAL, HEAL]
-        } else if (energy == 1050) {
-            return [MOVE, MOVE, MOVE, HEAL, HEAL, HEAL]
-        } else if (energy == 1100) {
-            return [MOVE, MOVE, MOVE, HEAL, HEAL, HEAL]
-        } else if (energy == 1150) {
-            return [MOVE, MOVE, MOVE, HEAL, HEAL, HEAL]
-        } else if (energy == 1200) {
-            return [MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL]
-        } else if (energy == 1250) {
-            return [MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL]
-        } else if (energy == 1300) {
-            return [MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL]
-        } else if (energy == 1350) {
-            return [MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL]
-        } else if (energy == 1400) {
-            return [MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL]
-        } else if (energy == 1450) {
-            return [MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL]
-        } else if (energy >= 1500) {
-            return [MOVE, MOVE, MOVE, MOVE, MOVE, HEAL, HEAL, HEAL, HEAL, HEAL]
-        }
-    }
+    //if (role == "DroneSeller") {
+    //    return [MOVE,CARRY];
+    //}
 
     if (role == "DroneMineralMiner") {
         if (energy >= 2300) {
@@ -352,6 +260,7 @@ let roleSpawn = {
         if (role == "Stormtrooper") newName = "Stormtrooper | ID [" + Game.time%1001 + "]";
         if (role == "ScoutTrooper") newName = "Scout trooper | ID [" + Game.time%1001 + "]";
         if (role == "DroneMineralMiner") newName = "AT-ST Walker | ID [" + Game.time%1001 + "]";
+        if (role == "DroneSeller") newName = "Merchant | ID [" + Game.time%1001 + "]";
         body = creepBody(amountEnergy, role);
         if (!Memory.room[spawn.room.name + ".amountIsLive." + "DroneRefiller"] && !Memory.room[spawn.room.name + ".amountIsLive." + "DroneBuilder"] && !Memory.room[spawn.room.name + ".amountIsLive." + "DroneUpgrader"] && spawn.room.energyCapacityAvailable > spawn.room.energyAvailable) {
             spawn.spawnCreep([MOVE, MOVE, CARRY, CARRY], newName, { memory: { role: "DroneRefiller" } });
