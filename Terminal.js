f7d607f19ef9a5d964934a361bca655d1b7be49b
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-08 19:35:05 +0300

    Фикс

--- Terminal.js
+++ Terminal.js
@@ -7,10 +7,10 @@ const Terminal = {
                         Game.market.calcTransactionCost(10000, room.name, order.roomName) < 10000);
                     Game.market.deal(orders[0].id, 10000, room.name);
                 }
-                if (room.terminal.store[RESOURCE_HYDROGEN] > 10000) {
+                if (room.terminal.store[RESOURCE_HYDROGEN] > 3000 && room.terminal.store[RESOURCE_ENERGY] >= 5000) {
                     const orders = Game.market.getAllOrders(order => order.resourceType == RESOURCE_HYDROGEN && order.type == ORDER_BUY && order.price > 0.02 &&
-                        Game.market.calcTransactionCost(10000, room.name, order.roomName) < 10000);
-                    Game.market.deal(orders[0].id, 10000, room.name);
+                        Game.market.calcTransactionCost(3000, room.name, order.roomName) < 5000);
+                    Game.market.deal(orders[0].id, 3000, room.name);
                 }
             }
         }
