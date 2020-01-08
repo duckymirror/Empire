68b92291fe912c3422d66d647d7ccb9a6e6539ef
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-08 15:51:34 +0300

    Фикс

--- Stormtrooper.js
+++ Stormtrooper.js
@@ -14,7 +14,7 @@ function killCreeps(creep) {
 function destroyStructures(creep) {
     const hostileTarget = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
         filter: (strc) => {
-            return strc.owner.username != "kotyara";
+            return strc.owner.username != "kotyara" && strc.structureType != "controller";
         }
     });
     if (creep.attack(hostileTarget) == ERR_NOT_IN_RANGE) creep.moveTo(hostileTarget);
@@ -24,29 +24,27 @@ let Stormtrooper = {
     /** @param {Creep} creep **/
     control(creep) {
         if (creep.spawning) {
-
             creep.memory.room = creep.room.name;
-            
         } else {
-        
             if (Game.flags.attack) {
                 if (Game.flags.attack.room != creep.room) {
                     creep.moveTo(Game.flags.attack);
                 } else {
+                    
                     const hostileCreeps = creep.room.find(FIND_HOSTILE_CREEPS, {
                         filter: (crps) => {
                             return crps.owner.username != "kotyara";
                         }
                     });
-                    if (hostileCreeps) {
+                    if (hostileCreeps.length > 0) {
                         killCreeps(creep);
                     } else {
-                        const hostileStructures = creep.pos.findClosestByPath(FIND_HOSTILE_STRUCTURES, {
+                        const hostileStructures = creep.room.find(FIND_HOSTILE_STRUCTURES, {
                             filter: (strc) => {
-                                return strc.owner.username != "kotyara";
+                                return strc.owner.username != "kotyara" && strc.structureType != "controller";
                             }
                         });
-                        if (hostileStructures) {
+                        if (hostileStructures.length > 0) {
                             destroyStructures(creep);
                         } else {
                             if (creep.room.controller && (!creep.room.controller.sign || (creep.room.controller.sign && creep.room.controller.sign.text != Memory.code))) {
