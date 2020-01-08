63028c76835bb11dfcd2a1517dc288679904270f
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-08 19:11:46 +0300

    Добавлен терминал

---
+++ Terminal.js
@@ -0,0 +1,19 @@
+const Terminal = {
+    control(room) {
+        if (room.controller && room.controller.my) {
+            if (room.terminal) {
+                if (room.terminal.store[RESOURCE_ENERGY] > 20000) {
+                    const orders = Game.market.getAllOrders(order => order.resourceType == RESOURCE_ENERGY && order.type == ORDER_BUY && order.price > 0.01 &&
+                        Game.market.calcTransactionCost(10000, room.name, order.roomName) < 10000);
+                    Game.market.deal(orders[0].id, 10000, room.name);
+                }
+                if (room.terminal.store[RESOURCE_HYDROGEN] > 10000) {
+                    const orders = Game.market.getAllOrders(order => order.resourceType == RESOURCE_HYDROGEN && order.type == ORDER_BUY && order.price > 0.02 &&
+                        Game.market.calcTransactionCost(10000, room.name, order.roomName) < 10000);
+                    Game.market.deal(orders[0].id, 10000, room.name);
+                }
+            }
+        }
+    }
+}
+module.exports = Terminal;
