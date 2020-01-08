63da0bf824006236b0726413525359b2477b0bbc
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-08 19:34:56 +0300

    Фикс

--- DroneSeller.js
+++ DroneSeller.js
@@ -7,7 +7,9 @@ function getResource(creep) {
     if (droppedEnergy.length > 0) {
         if (creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) creep.moveTo(droppedEnergy[0], { heuristicWeight: 1.2, range: 1, reusePath: 50 });
     } else {
-        if (creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] > 100000+creep.store.getCapacity()) {
+        if (creep.room.terminal.store[RESOURCE_ENERGY] < 5000) {
+            if (creep.withdraw(creep.room.storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) creep.moveTo(creep.room.storage, { heuristicWeight: 1.2, range: 1, reusePath: 50 });
+        } else if (creep.room.storage && creep.room.storage.store[RESOURCE_ENERGY] > 100000+creep.store.getCapacity()) {
             if (creep.withdraw(creep.room.storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) creep.moveTo(creep.room.storage, { heuristicWeight: 1.2, range: 1, reusePath: 50 });
         } else if (creep.room.storage && creep.room.storage.store[RESOURCE_HYDROGEN] > 10000+creep.store.getCapacity()) {
             if (creep.withdraw(creep.room.storage, RESOURCE_HYDROGEN) == ERR_NOT_IN_RANGE) creep.moveTo(creep.room.storage, { heuristicWeight: 1.2, range: 1, reusePath: 50 });
@@ -18,11 +20,11 @@ function getResource(creep) {
 
 function sell(creep) {
     if (creep.room.terminal) {
-        if (creep.getUsedCapacity(RESOURCE_ENERGY) > 0) {
+        if (creep.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
             if (creep.transfer(creep.room.terminal, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.terminal, { heuristicWeight: 1.2, range: 1, reusePath: 20 });
             }
-        } else if (creep.getUsedCapacity(RESOURCE_HYDROGEN) > 0) {
+        } else if (creep.store.getUsedCapacity(RESOURCE_HYDROGEN) > 0) {
             if (creep.transfer(creep.room.terminal, RESOURCE_HYDROGEN) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.terminal, { heuristicWeight: 1.2, range: 1, reusePath: 20 });
             }
