4d239031754ada5599a3e57be649b6f11848394a
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-24 19:50:37 +0300

    Фикс

--- Stormtrooper.js
+++ Stormtrooper.js
@@ -47,9 +47,8 @@ let Stormtrooper = {
                         if (hostileStructures.length > 0) {
                             destroyStructures(creep);
                         } else {
-                            if (creep.room.controller && (!creep.room.controller.sign || (creep.room.controller.sign && creep.room.controller.sign.text != Memory.code))) {
-                                if (creep.signController(creep.room.controller, Memory.code) == ERR_NOT_IN_RANGE) creep.moveTo(creep.room.controller)
-                            }
+                            creep.say("!")
+                            creep.moveTo(Game.flags.Attack);
                         }
                     }
                 }
