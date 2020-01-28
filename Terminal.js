b268ac956d14a2004ccea47748237dcf4e193c4b
Igor Shaposhnikov <jourloy@yandex.ru>
2020-01-28 17:59:16 +0300

    Added auto-buying energy

--- Terminal.js
+++ Terminal.js
@@ -2,6 +2,19 @@ const Terminal = {
     control(room) {
         if (room.controller && room.controller.my) {
             if (room.terminal) {
+                if (room.terminal.store[RESOURCE_ENERGY] < 30001) {
+                    const orders = Game.market.getAllOrders(order => order.roomName == room.name && order.type == ORDER_BUY && order.resourceType == RESOURCE_ENERGY);
+                    if (!orders) {
+                        let amount = 30001 - room.terminal.store[RESOURCE_ENERGY]
+                        Game.market.createOrder({
+                            type: ORDER_BUY,
+                            resourceType: RESOURCE_ENERGY,
+                            price: 0.01,
+                            totalAmount: amount,
+                            roomName: room.name
+                        });
+                    }
+                }
                 if (room.terminal.store[RESOURCE_ENERGY] > 20000) {
                     const orders = Game.market.getAllOrders(order => order.resourceType == RESOURCE_ENERGY && order.type == ORDER_BUY && order.price > 0.015 &&
                         Game.market.calcTransactionCost(10000, room.name, order.roomName) < 10000);
